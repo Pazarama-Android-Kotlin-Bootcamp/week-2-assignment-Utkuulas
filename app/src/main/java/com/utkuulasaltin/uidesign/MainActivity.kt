0f77bc3ffd8f2e084619a89e705cc241eed974b7
Utku <utkuulasaltin@gmail.com>
2022-09-29 01:07:00 +0300

    added new files

--- app/src/main/java/com/utkuulasaltin/uidesign/MainActivity.kt
+++ app/src/main/java/com/utkuulasaltin/uidesign/MainActivity.kt
@@ -2,10 +2,24 @@ package com.utkuulasaltin.uidesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.ImageView
 class MainActivity : AppCompatActivity() {
+    private lateinit var ivChangePasswordVisibility: ImageView
+    private var isVisibilityOn = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ivChangePasswordVisibility  = findViewById(R.id.imageViewShowPassword)
+
+        ivChangePasswordVisibility.setOnClickListener {
+            if (isVisibilityOn) {
+                ivChangePasswordVisibility.setImageResource(R.drawable.ic_baseline_visibility_24)
+                isVisibilityOn = false
+            } else {
+                ivChangePasswordVisibility.setImageResource(R.drawable.ic_baseline_visibility_off_24)
+                isVisibilityOn = true
+            }
+        }
     }
 }
